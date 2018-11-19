fcdf614f1d6e1528e9c11b7e162e5da6c8fa720a
Nick Allevato <nicholas.allevato@gmail.com>
2018-11-19 12:56:42 -0500

    Example files and command line file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+use std::fs::File;
+use std::io::ErrorKind;
+use std::io::{Error, Read};
+
+fn read_file(file_name: String) -> std::result::Result<String, Error> {
+  let mut file = File::open(file_name)?;
+
+  let mut buffer = Vec::new();
+  file.read_to_end(&mut buffer)?;
+
+  Ok(String::from_utf8(buffer).unwrap())
+}
+
 fn main() {
-    println!("Hello, world!");
+  let args: Vec<String> = env::args().collect();
+  let file_name: String = args[1].parse().unwrap();
+
+  match read_file(file_name) {
+    Err(_e) => {
+      println!("File not found");
+    }
+    Ok(contents) => {
+      for c in contents.chars() {
+        print!("{}", c);
+      }
+    }
+  }
+
+  println!("Hello, world!");
 }
