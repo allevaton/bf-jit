fecf736220539c2353572d3cd6f5217f80cee602
Nick Allevato <nicholas.allevato@gmail.com>
2018-11-19 15:40:55 -0500

    Tokenize and scaffolding of optimizer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::{Error, Read};
+use std::result::Result;
 
 struct Token {}
 
@@ -18,13 +19,44 @@ impl SubToken {
   }
 }
 
-fn read_file(file_name: String) -> std::result::Result<String, Error> {
+struct Cell {
+  data: u64,
+}
+
+struct Instruction {}
+
+fn read_file(file_name: String) -> Result<String, Error> {
   let mut file = File::open(file_name)?;
 
   let mut buffer = Vec::new();
   file.read_to_end(&mut buffer)?;
 
   Ok(String::from_utf8(buffer).unwrap())
+}
+
+fn tokenize(contents: String) -> Vec<Token> {
+  let mut tokens: Vec<Token> = Vec::new();
+  for c in contents.chars() {
+    match c {
+      '+' => {
+        tokens.push(AddToken::new());
+        println!("ADD");
+      }
+      '-' => {
+        tokens.push(SubToken::new());
+        println!("SUBTRACT");
+      }
+      _ => {
+        continue;
+      }
+    }
+  }
+
+  tokens
+}
+
+fn optimize(tokens: Vec<Token>) -> Vec<Instruction> {
+  Vec::new()
 }
 
 fn main() {
@@ -36,22 +68,9 @@ fn main() {
       println!("File not found");
     }
     Ok(contents) => {
-      let mut tokens: Vec<Token> = Vec::new();
-      for c in contents.chars() {
-        match c {
-          '+' => {
-            tokens.push(AddToken::new());
-            println!("ADD");
-          }
-          '-' => {
-            tokens.push(SubToken::new());
-            println!("SUBTRACT");
-          }
-          _ => {
-            continue;
-          }
-        }
-      }
+      //      let mut cells: Vec<Cell> = Vec::new();
+      let tokens = tokenize(contents);
+      let optimized_instructions = optimize(tokens);
     }
   }
 
